96ce66bb5174a3a5d12ba7f2a2026d4aaaa9f065
dongshu2013 <dongs2011@gmail.com>
2023-03-03 14:15:49 -0800

    add some useful scripts

--- scripts/checkstate.ts
+++ scripts/checkstate.ts
@@ -1,13 +1,31 @@
-
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
+import * as config from '../config.json';
 
 async function main() {
-    const erc721 : ethers.Contact = await ethers.getContractAt(
-      "HexlinkErc721Impl", "0x6592915622D7EBaf3cB1da112F71Fc52a36616c3"
+    const netConf = config[network.name as keyof typeof config] || {};
+    const oracles = netConf["oracles"] || {};
+    const emailOtpOracle = await ethers.getContractAt(
+        "SimpleIdentityOracle",
+        oracles["EMAIL_OTP"], // email otp
     );
-    
-    const deposit = await erc721.gasSponsorship();
-    console.log(deposit.toString());
+    const twitterOAuthOracle = await ethers.getContractAt(
+        "SimpleIdentityOracle",
+        oracles["TWITTER_OAUTH"] // twitter oauth
+    );
+
+    const validator = new ethers.Wallet(process.env.HARDHAT_VALIDATOR);
+    const kmsPubAddr1 = "0x943fabe0d1ae7130fc48cf2abc85f01fc987ec81";
+    const kmsPubAddr2 = "0x72cf1663d40c5215e5f9607df3464becdffd59d9";
+
+    console.log("Email OTP Oracle: ");
+    console.log(await emailOtpOracle.isRegistered(validator.address));
+    console.log(await emailOtpOracle.isRegistered(kmsPubAddr1));
+    console.log(await emailOtpOracle.isRegistered(kmsPubAddr2));
+
+    console.log("Twitter OAuth Oracle: ");
+    console.log(await twitterOAuthOracle.isRegistered(validator.address));
+    console.log(await twitterOAuthOracle.isRegistered(kmsPubAddr1));
+    console.log(await twitterOAuthOracle.isRegistered(kmsPubAddr2));
 }
 
 // We recommend this pattern to be able to use async/await everywhere

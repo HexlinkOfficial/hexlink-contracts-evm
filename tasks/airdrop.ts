962a017416ad51d07205640cbacc94ebebb46a25
dongshu2013 <dongs2011@gmail.com>
2023-10-01 14:24:08 -0700

    support custom airdrop validator

--- tasks/airdrop.ts
+++ tasks/airdrop.ts
@@ -7,12 +7,14 @@ task("create_campaign", "create new campaign")
     .addOptionalParam("token")
     .addOptionalParam("endat")
     .addOptionalParam("amount")
+    .addOptionalParam("validator")
     .setAction(async (args, hre : HardhatRuntimeEnvironment) => {
         const airdrop = await getAirdrop(hre);
         const airdropAddr = await airdrop.getAddress();
         const {deployer} = await hre.ethers.getNamedSigners();
         const token = args.token ?? "0xEC5dCb5Dbf4B114C9d0F65BcCAb49EC54F6A0867";
         const amount = BigInt(args.amount ?? 10n ** 18n);
+        const validator = args.validator ?? deployer.address;
         const erc20 = new Contract(
             token,
             [
@@ -26,14 +28,15 @@ task("create_campaign", "create new campaign")
             console.log("Approving token...");
             await erc20.approve(airdropAddr, amount);
         }
-        console.log("Creating campaign...");
-        const tx = await airdrop.createCampaign({
+        const campaign = {
             token,
             startAt: 0,
             endAt: args.endAt ?? "0xffffffffffff",
             deposit: amount,
-            owner: await getValidator(hre, "hexlinkValidator"),
-        });
+            owner: validator,
+        };
+        console.log("Creating campaign: ", campaign);
+        const tx = await airdrop.createCampaign(campaign);
         console.log("Tx sent: ", tx.hash);
         const receipt = await tx.wait();
         console.log("Tx mined: ", receipt);
